dc1d7508df7c717e3cedc9364bda59bd93d0cb44
michzuerch <michzuerch@gmail.com>
2018-02-19 14:09:06 +0100

    Beginn mit der Arbeit...

--- src/main/java/com/gmail/michzuerch/anouman/presentation/ui/AboutView.java
+++ src/main/java/com/gmail/michzuerch/anouman/presentation/ui/AboutView.java
@@ -4,9 +4,11 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.cdi.CDIView;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.HorizontalLayout;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.vaadin.teemusa.flexlayout.*;
 import org.vaadin.viritin.label.RichText;
 
 import javax.annotation.PostConstruct;
@@ -14,18 +16,33 @@ import javax.inject.Inject;
 
 @Theme("anouman")
 @CDIView("AboutView")
-public class AboutView extends VerticalLayout implements View {
+public class AboutView extends HorizontalLayout implements View {
     private static Logger logger = LoggerFactory.getLogger(AboutView.class.getName());
 
     @Inject
     Menu menu;
 
+
+    private Component createContent() {
+        FlexLayout layout = new FlexLayout();
+
+        layout.setFlexDirection(FlexDirection.Column);
+        layout.setAlignItems(AlignItems.FlexStart);
+        layout.setJustifyContent(JustifyContent.SpaceAround);
+        layout.setAlignContent(AlignContent.FlexStart);
+        layout.setFlexWrap(FlexWrap.Nowrap);
+
+        layout.addComponents(new RichText().withMarkDownResource("/about.md"));
+        layout.setSizeFull();
+        return layout;
+    }
+
+
     @PostConstruct
     void init() {
         logger.debug("AboutView init");
-        RichText richText = new RichText().withMarkDownResource("/about.md");
-        addComponents(menu);
-        addComponentsAndExpand(richText);
+        addComponent(createContent());
+        setSizeFull();
     }
 
     @Override
--- src/main/java/com/gmail/michzuerch/anouman/presentation/ui/AnoumanUI.java
+++ src/main/java/com/gmail/michzuerch/anouman/presentation/ui/AnoumanUI.java
@@ -1,5 +1,6 @@
 package com.gmail.michzuerch.anouman.presentation.ui;
 
+import com.vaadin.annotations.PreserveOnRefresh;
 import com.vaadin.annotations.Push;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
@@ -8,8 +9,13 @@ import com.vaadin.cdi.CDIViewProvider;
 import com.vaadin.navigator.Navigator;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.shared.communication.PushMode;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
 import org.slf4j.LoggerFactory;
+import org.vaadin.teemusa.flexlayout.FlexLayout;
 
 import javax.inject.Inject;
 
@@ -19,18 +25,54 @@ import javax.inject.Inject;
 @Theme("anouman")
 @Title("Anouman")
 @Push(PushMode.AUTOMATIC)
+@PreserveOnRefresh
 @SuppressWarnings("serial")
 @CDIUI("")
+
 public class AnoumanUI extends UI {
     private static org.slf4j.Logger logger = LoggerFactory.getLogger(AnoumanUI.class.getName());
 
     @Inject
-    private CDIViewProvider viewProvider;
+    private CDIViewProvider cdiViewProvider;
+
+//    @Override
+//    protected void init(VaadinRequest request) {
+//        Navigator navigator = new Navigator(this, this);
+//        navigator.addProvider(cdiViewProvider);
+//        navigator.navigateTo("AboutView");
+//    }
 
     @Override
     protected void init(VaadinRequest request) {
-        Navigator navigator = new Navigator(this, this);
-        navigator.addProvider(viewProvider);
+        Panel container = new Panel();
+
+        Navigator navigator = new Navigator(this, container);
+        navigator.addProvider(cdiViewProvider);
+//        navigator.addView("fiddle", FiddleView.class);
+//        navigator.addView("example", ExampleView.class);
+
+        FlexLayout naviBar = FlexLayout.create().horizontal().justifyContent()
+                .spaceAround().alignContent().end().nowrap().build();
+
+        naviBar.addComponents(createNaviButton("AboutView"),
+                createNaviButton("AdresseView"));
+        naviBar.setHeight("41px");
+
+        VerticalLayout content = new VerticalLayout(naviBar, container);
+        setContent(content);
         navigator.navigateTo("AboutView");
+
+        setSizeFull();
+        content.setSizeFull();
+        container.setSizeFull();
+
+        content.setExpandRatio(container, 1.0f);
+    }
+
+    public Button createNaviButton(String viewName) {
+        Button button = new Button(viewName,
+                e -> getNavigator().navigateTo(viewName));
+        button.setStyleName(ValoTheme.BUTTON_LINK);
+        return button;
     }
 }
